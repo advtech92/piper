243622eddaa360771769ad440d2d1ace6e9abc47
Dani <dansapelli@yahoo.com>
2024-02-07 14:12:23 -0500

    Added some error catches to the discord code for lack of perms

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from logger import logger
 from dotenv import load_dotenv
 from database import insert_message, has_introduced, record_introduction
 from config import DISCORD_TOKEN
+from discord.errors import HTTPException, Forbidden
 
 # Setup our bot Piper!
 logger.info("Starting Piper...")
@@ -14,20 +15,34 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    logger.info(f'{client.user} has connected to Discord!')
     for guild in client.guilds:
         if not has_introduced(str(guild.id)):
-            default_channel = next((channel for channel in guild.text_channels if channel.permissions_for(guild.me).send_messages), None)
-            if default_channel:
-                await default_channel.send("Hi there! I'm Piper, a bit of a shy observer here to learn from all the wonderful conversations happening. I promise to quietly listen and keep everything between us. I won't chime in for now, just soaking it all in. Thanks for letting me be a part of your space! 📚✨")
-                record_introduction(str(guild.id))
+            try:
+                default_channel = next((channel for channel in guild.text_channels if channel.permissions_for(guild.me).send_messages), None)
+                if default_channel:
+                    await default_channel.send("Hi there! I'm Piper, a bit of a shy observer here to learn from all the wonderful conversations happening. I promise to quietly listen and keep everything between us. I won't chime in for now, just soaking it all in. Thanks for letting me be a part of your space! 📚✨")
+                    record_introduction(str(guild.id))
+            except HTTPException as e:
+                logger.error(f'HTTPException while sending introduction message: {e}')
+            except Forbidden as e:
+                logger.error(f'Forbidden: Cannot send introduction message due to lack of permissions: {e}')
+            except Exception as e:
+                logger.error(f'Unexpected error while sending introduction message: {e}')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    # Store message in the database
-    insert_message(message.content, message.author.id, message.channel.id)
+    try:
+        # Store message in the database
+        insert_message(message.content, message.author.id, message.channel.id)
+    except Exception as e:
+        logger.error(f'Unexpected error while processing message: {e}')
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    logger.error(f'Unhandled exception in {event}', exc_info=True)
 
 load_dotenv()
 client.run(DISCORD_TOKEN)
